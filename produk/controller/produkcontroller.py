1c8fbd23f07a7fb6659fd9e106ab6bb8145baf58
Yudi <yudi.krazy88@gmail.com>
2024-05-15 20:19:37 +0700

    second commit

--- produk/controller/produkcontroller.py
+++ produk/controller/produkcontroller.py
@@ -40,3 +40,92 @@ class ProdukController:
             return {'produk': produk_data}, 200
         else:
             return {'message': 'Terjadi kesalahan saat mengambil data produk'}, 500
+        
+    # pencarian data dari tabel produk
+    def cari_produk(self,id):
+        try:
+            cursor = self.db.cursor(dictionary=True)
+            cursor.execute("select * from produk where id = %s", (id,))
+            produk = cursor.fetchone()
+            cursor.close()
+
+            if produk:
+                created_at = produk['created_at']
+                updated_at = produk['updated_at']
+                if isinstance(created_at, timedelta):
+                    created_at = datetime.now() - created_at
+                if isinstance(updated_at, timedelta):
+                    updated_at = datetime.now() - updated_at
+                
+                produk_obj = Produk(produk['id'], produk['nama'], produk['kategori'], produk['harga'], produk['stok'], created_at, updated_at)
+                return {'produk': produk_obj.to_dict()}, 200
+            else:
+                return {'message': 'Data produk tidak ditemukan'}, 404
+        except mysql.connector.Error as e:
+            print(f"Error: {e}")
+            return {'message': 'Terjadi kesalahan saat mencari data produk'}, 500
+        
+    # tambah data produk
+    def tambah_produk(self,data):
+        try:
+            nama = data.get('nama')
+            kategori = data.get('kategori')
+            harga = data.get('harga')
+            stok = data.get('stok')
+            created_at = datetime.now()
+            updated_at = datetime.now()
+
+            cursor = self.db.cursor()
+            query = 'insert into produk (nama, kategori, harga, stok, created_at, updated_at) VALUES (%s, %s, %s, %s, %s, %s)'
+            cursor.execute(query, (nama, kategori, harga, stok, created_at, updated_at))
+            self.db.commit()
+            cursor.close()
+
+            return {'message': 'Data produk berhasil ditambahkan'}, 201
+        except mysql.connector.Error as e:
+            print(f"Error: {e}")
+            return {'message': 'Terjadi kesalahan saat menambah data produk'}, 500
+        
+    # update data produk
+    def update_produk(self, id, data):
+        try:
+            if not data:
+                return {'message': 'Data yang diterima kosong'}, 400
+            
+            cursor = self.db.cursor(dictionary=True)
+            query = "select * from produk where id = %s"
+            cursor.execute(query, (id,))
+            guru = cursor.fetchone()
+            cursor.close()
+
+            if not guru:
+                return {'message': 'Data produk tidak ditemukan'}, 404
+            
+            cursor = self.db.cursor()
+            query = "update produk SET nama = %s, kategori = %s, harga = %s, stok = %s, updated_at = NOW() where id = %s"
+            cursor.execute(query, (data['nama'], data['kategori'], data['harga'], data['stok'], id))
+            self.db.commit()
+            cursor.close()
+
+            return {'message': 'Data produk berhasil diperbarui'}, 200
+        except mysql.connector.Error as e:
+            print(f"Error: {e}")
+            return {'message': 'Terjadi kesalahan saat memperbarui data produk'}, 500
+        
+    # delete data produk
+    def hapus_produk(self, id):
+        try:
+            cursor = self.db.cursor()
+            query = "delete from produk where id = %s"
+            cursor.execute(query, (id,))
+            self.db.commit()
+            affected_rows = cursor.rowcount
+            cursor.close()
+
+            if affected_rows > 0:
+                return {'message': 'Data Produk berhasil dihapus'}, 200
+            else:
+                return {'message': 'Data Produk tidak ditemukan'}, 404
+        except mysql.connector.Error as e:
+            print(f"Error: {e}")
+            return {'message': 'Terjadi kesalahan saat menghapus data produk'}, 500
--- produk/routes_produk.py
+++ produk/routes_produk.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, render_template
 from produk.controller.produkcontroller import ProdukController
 
 produk_routes = Blueprint('produk_routes',__name__)
@@ -8,3 +8,26 @@ produk_controller = ProdukController()
 def lihat_produk():
     if request.method == 'GET':
         return jsonify(produk_controller.lihat_produk())
+    
+#rute mencari data
+@produk_routes.route('/produk/<int:id>', methods=['GET'])
+def cari_produk(id):
+    return produk_controller.cari_produk(id)
+
+#rute tambah data
+@produk_routes.route('/produk', methods=['POST'])
+def tambah_produk():
+    data = request.json
+    return produk_controller.tambah_produk(data)
+
+#rute update data
+@produk_routes.route('/produk/<int:id>', methods=['PUT'])
+def update_produk(id):
+    data = request.json
+    return produk_controller.update_produk(id, data)
+
+#rute hapus data
+@produk_routes.route('/produk/<int:id>', methods=['DELETE'])
+def hapus_produk(id):
+    data = request.json
+    return produk_controller.hapus_produk(id)
